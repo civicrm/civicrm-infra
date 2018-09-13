1b6c7b4f10be72a72a24832f8acea80370c98667
Tim Otten <totten@civicrm.org>
2018-09-12 22:05:40 -0700

    CiviCRM-PR-Cleanup - Run on bknix

--- jenkins-examples/CiviCRM-PR-Cleanup.bash
+++ jenkins-examples/CiviCRM-PR-Cleanup.bash
@@ -2,21 +2,16 @@
 set -ex
 
 if [ -e $HOME/.profile ]; then . $HOME/.profile; fi
+for BKPROF in dfl min max ; do
+  (
+    eval $(use-bknix "$BKPROF")
+    if [ -z "$BKITBLD" ]; then echo "Invalid BKPROF"; exit 1; fi
 
-if [ -d "/home/jenkins/buildkit" ]; then
-  PRJDIR="/home/jenkins/buildkit"
-elif [ -d "/srv/buildkit" ]; then
-  PRJDIR="/srv/buildkit"
-else
-  echo "Failed to find PRJDIR"
-  exit 1
-fi
-
-export PATH="$PRJDIR/bin:$PATH"
-
-## Expiration: one week == 7*24 == 168
-## Redundancy: 8 hours
-# [ML] 2018-01-19 lowered to 5 days, 4 hours
-find-stale-builds "$PRJDIR/build" 120 4 | while read BLD ; do
-  echo y | civibuild destroy $(basename $BLD)
+    ## Expiration Period (ie all builds destroyed after...):           one week == 7*24 == 168 hours
+    ## Redundancy Period (ie duplicate PR builds destroyed after...):  8 hours
+    # [ML] 2018-01-19 lowered to 5 days, 4 hours
+    find-stale-builds "$BKITBLD" 120 4 | while read BLD ; do
+      echo y | civibuild destroy $(basename $BLD)
+    done
+  )
 done
