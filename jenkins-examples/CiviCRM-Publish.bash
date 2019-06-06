6da118b4473c7e5419310a1f7d935e6fb138b718
Tim Otten <totten@civicrm.org>
2019-06-05 23:03:37 -0700

    CiviCRM-Publish.bash - Update to work on one branch at a time
    We need the ability to use different PHP environments with different
    branches, so the scope of this job has been narrowed to just build one
    branch at a time.
    
    Other jobs will be responsible for firing it.

--- jenkins-examples/CiviCRM-Publish.bash
+++ jenkins-examples/CiviCRM-Publish.bash
@@ -1,9 +1,30 @@
 #!/bin/bash
+## INPUTS: branchName - Ex: "5.13", "5.14", "master"
+
 set -ex
 
+## Determine which version of bknix to load
+function pick_bknix() {
+  case "$branchName" in
+    4.6|5.7|5.13)
+      use-bknix old
+      ;;
+    *)
+      use-bknix min
+      ;;
+  esac
+}
+
+
 if [ -e $HOME/.profile ]; then . $HOME/.profile; fi
-eval $(use-bknix min)
+eval $(pick_bknix)
 if [ -z "$BKITBLD" ]; then echo "Invalid BKPROF"; exit 1; fi
+
+echo "== Platform versions =="
+php --version
+mysql --version
+echo
+echo
 
 export FILE_SUFFIX=$( date -u '+%Y%m%d%H%M' )
 ## Note: Not safe for concurrent build
@@ -12,23 +33,21 @@ BLDDIR="$BKITBLD/$BLDNAME"
 OUTDIR="$WORKSPACE/output"
 
 ## Validate inputs
-if [[ $branchNames =~ ^[a-zA-Z0-9\ \.\-]+$ ]] ; then
+if [[ $branchName =~ ^[a-zA-Z0-9\.\-]+$ ]] ; then
   echo "Branch name looks OK"
 else
   echo "Malformed branch name"
   exit 1
 fi
 
-for branchName in $branchNames ; do
-  if [ "$branchName" = "4.6" ]; then
-    echo 'CREATE DATABASE IF NOT EXISTS nothing;GRANT SELECT ON `nothing`.* TO "nobody"@"localhost" IDENTIFIED BY "thesearentthedroidsyourelookingfor";GRANT SELECT ON `nothing`.* TO "nobody"@"127.0.0.1" IDENTIFIED BY "thesearentthedroidsyourelookingfor";' | amp sql -a
-  
-    if ! php -r 'error_reporting(E_ALL); $v = mysql_real_escape_string("sql ok"); echo $v;' | grep -q 'sql ok' ; then
-      echo "Cannot build CiviCRM 4.6 on this host. Configure PHP-MySQL default privileges."
-      exit 1  
-    fi
+if [ "$branchName" = "4.6" ]; then
+  echo 'CREATE DATABASE IF NOT EXISTS nothing;GRANT SELECT ON `nothing`.* TO "nobody"@"localhost" IDENTIFIED BY "thesearentthedroidsyourelookingfor";GRANT SELECT ON `nothing`.* TO "nobody"@"127.0.0.1" IDENTIFIED BY "thesearentthedroidsyourelookingfor";' | amp sql -a
+
+  if ! php -r 'error_reporting(E_ALL); $v = mysql_real_escape_string("sql ok"); echo $v;' | grep -q 'sql ok' ; then
+    echo "Cannot build CiviCRM 4.6 on this host. Configure PHP-MySQL default privileges."
+    exit 1
   fi
-done
+fi
 
 ## Ensure the build exists and has clean data dir
 if [ ! -d "$BLDDIR" ]; then
@@ -41,12 +60,8 @@ mkdir -p "$BLDDIR/web"
 ## Build the tarballs
 pushd "$BLDDIR"
   cividist update
-  remoteBranchNames=""
-  for branchName in $branchNames ; do
-    remoteBranchNames="${remoteBranchNames} origin/${branchName}"
-  done
-  cividist build $remoteBranchNames
-  
+  cividist build "origin/${branchName}"
+
   ## Don't need these files on gcloud
   find web -name README.html -delete
   find web -name .htaccess -delete
