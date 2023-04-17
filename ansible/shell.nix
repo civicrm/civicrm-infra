49da8530c827e64cd0fe93cdc94a6b4f158f7672
Tim Otten <totten@civicrm.org>
2023-04-17 14:40:00 -0700

    shell.nix - Pin to nixpkgs 22.05

--- ansible/shell.nix
+++ ansible/shell.nix
@@ -1,6 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
+# { pkgs ? import <nixpkgs> {} }:
+## For unclear reasons, when using nixpkgs `22.11` and `unstable`, I'm
+## having trouble with locale data. So "pkgs" is pinned to 22.05.
 
 let
+
+  pkgs = import(fetchTarball { ## nixos-22.05
+    url = "https://github.com/nixos/nixpkgs/archive/ce6aa13369b667ac2542593170993504932eb836.tar.gz";
+    sha256 = "0d643wp3l77hv2pmg2fi7vyxn4rwy0iyr8djcw1h5x72315ck9ik";
+  }) {};
 
   ## Python Setup - Some Ansible plugins may require extra packages.
   myPythonPackages = p: with p; [
